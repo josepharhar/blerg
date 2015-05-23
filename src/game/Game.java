a12ea11555bcef09f64e1c4050f2daff6d756395
jono.chadwell <jono.chadwell@gmail.com>
2015-05-23 02:33:22 -0700

    Added task for joey

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,26 @@
 package game;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Game {
+	
+	public void startGame() {
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+
+			@Override
+			public void run() {
+				updateGameState();
+			}
+			
+		};
+		
+		timer.scheduleAtFixedRate(task, 100, 100);
+	}
+	
+	private void updateGameState() {
+		
+	}
     
 }
