020b25b9ee6ba908ee22cd8f66259b37b855ccb2
jono.chadwell <jono.chadwell@gmail.com>
2015-05-23 02:42:48 -0700

    networking prototype

---
+++ src/networking/Networking.java
@@ -0,0 +1,15 @@
+package networking;
+
+import game.Entity;
+
+import java.util.List;
+
+public class Networking {
+	public void start() {
+		
+	}
+	
+	public List<Entity> getLatestState() {
+		return null;
+	}
+}
