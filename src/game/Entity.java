5142fa3beeceda8fd0ca76282328f2650dbd120f
Joseph Arhar <josepharhar@gmail.com>
2015-05-23 01:40:08 -0700

    added game package

---
+++ src/game/Entity.java
@@ -0,0 +1,19 @@
+package game;
+
+public interface Entity {
+    
+    /**
+     * Get the x location of this entity
+     */
+    public double getx();
+    
+    /**
+     * Get the y location of this entity
+     */
+    public double gety();
+    
+    /**
+     * Get the radius of this entity, since all entities are circles
+     */
+    public double getr();
+}
---
+++ src/game/Player.java
@@ -0,0 +1,21 @@
+package game;
+
+public class Player implements Entity {
+    
+    private double x;
+    private double y;
+    private double r;
+    
+    public double getx() {
+        return x;
+    }
+    
+    public double gety() {
+        return y;
+    }
+    
+    public double getr() {
+        return r;
+    }
+
+}
